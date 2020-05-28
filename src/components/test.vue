df63bad7c8cee3c17babc9d9b534396391ccf5f7
BarsTK <zinin_uladzimir@epam.com>
2020-05-29 02:07:05 +0300

    Start commit

---
+++ src/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <div>{{obj.id}}</div>
+  <div>{{caption}}</div>
+  <router-link :to="{name: 'notes'}" tag="button" class="btn">Back to list without saving</router-link>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "test",
+    props: {obj: Object}
+  }
+</script>
+
+<style scoped>
+</style>
